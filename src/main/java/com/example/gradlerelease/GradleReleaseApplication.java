56ac3cc158e0cc8c8525ed19291f0528ffca1dd9
Diogo Fabrile <diogo@asaitec.eu>
2020-02-24 12:54:37 +0100

    firstCOMMIT

---
+++ src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlerelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleReleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleReleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradlerelease/GradleReleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlerelease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleReleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
