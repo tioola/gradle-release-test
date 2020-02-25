aa29c0ac14e27155d49932f3fddbad335891230d
Diogo Fabrile <diogo@asaitec.eu>
2020-02-25 10:58:54 +0100

    CSPROC-1003 New feature 003

--- src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
+++ src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradleReleaseApplication {
-//Change7
+//Change8
     public static void main(String[] args) {
         SpringApplication.run(GradleReleaseApplication.class, args);
     }
