9efcd66e7bcd94817ab913f9b72541d37649a647
Diogo Fabrile <diogo@asaitec.eu>
2020-02-25 10:18:02 +0100

    CSPROC-1002 First change

--- src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
+++ src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradleReleaseApplication {
-//Change5
+//Change6
     public static void main(String[] args) {
         SpringApplication.run(GradleReleaseApplication.class, args);
     }
