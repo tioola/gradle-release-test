3c28911d1915d2199b6f5a19398424e2a4a3c195
Diogo Fabrile <diogo@asaitec.eu>
2020-03-11 15:28:05 +0100

    CSPROC-5002 New 2 feature

--- src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
+++ src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradleReleaseApplication {
-    //Change16
+    //Change17
     public static void main(String[] args) {
         SpringApplication.run(GradleReleaseApplication.class, args);
     }
