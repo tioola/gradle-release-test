27c7ccfcf31a268b6a36dda6ce601b40b60b0431
Diogo Fabrile <diogo@asaitec.eu>
2020-03-11 15:28:26 +0100

    CSPROC-5002 Review

--- src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
+++ src/main/java/com/example/gradlerelease/GradleReleaseApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GradleReleaseApplication {
-    //Change17
+    //Change18
     public static void main(String[] args) {
         SpringApplication.run(GradleReleaseApplication.class, args);
     }
